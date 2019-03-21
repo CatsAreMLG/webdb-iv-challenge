4528158fb468a788c0f01a457be12326f70c4748
CatsAreMLG <joshuadgon@gmail.com>
2019-03-21 17:28:49 -0500

    Initialize express server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const knex = require('knex')
+const server = express()
+const helmet = require('helmet')
+const PORT = 9090
+
+server.use(helmet())
+server.use(express.json())
+
+server.get('/', (req, res) => res.send('<h2>Welcome to the API</h2>'))
+
+server.listen(PORT, _ => console.log('Listening on port ' + PORT))
