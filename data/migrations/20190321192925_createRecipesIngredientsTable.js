5d2ff7015947e637dd040e65297cca7e20272f3d
CatsAreMLG <joshuadgon@gmail.com>
2019-03-21 19:36:05 -0500

    Create recipes ingredients table

---
+++ data/migrations/20190321192925_createRecipesIngredientsTable.js
@@ -0,0 +1,15 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('recipes_ingredients', table => {
+    table.increments()
+    table.integer('recipe_id').notNullable()
+    table.integer('ingredient_id').notNullable()
+    table
+      .float('quantity')
+      .notNullable()
+      .unsigned()
+  })
+}
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('recipes_ingredients')
+}
