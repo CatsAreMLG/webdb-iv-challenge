7d236d3eaa786ac620b3d254ac82bc9025a6de26
CatsAreMLG <joshuadgon@gmail.com>
2019-03-21 20:57:26 -0500

    Add ingredients endpoints

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const helmet = require('helmet')
 const server = express()
 const DishesRouter = require('./data/routers/dishesRouter')
 const RecipesRouter = require('./data/routers/recipesRouter')
+const IngredientsRouter = require('./data/routers/ingredientsRouter')
 const PORT = 9090
 
 server.use(helmet())
 server.use(express.json())
 server.use('/api/dishes', DishesRouter)
 server.use('/api/recipes', RecipesRouter)
-server.use('/api/recipes', RecipesRouter)
+server.use('/api/ingredients', IngredientsRouter)
 
 server.get('/', (req, res) => res.send('<h2>Welcome to the API</h2>'))
 
