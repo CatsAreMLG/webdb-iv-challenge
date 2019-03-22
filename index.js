100a056526debba70d3aebde287fd4ddaa978968
CatsAreMLG <joshuadgon@gmail.com>
2019-03-21 19:55:02 -0500

    Add get dishes endpoint

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const knex = require('knex')
-const server = express()
 const helmet = require('helmet')
+const server = express()
 const db = require('./dbConfig')
-
+const knexConfig = require('./knexfile')
+const DishesRouter = require('./data/routers/dishesRouter')
 const PORT = 9090
 
 server.use(helmet())
 server.use(express.json())
+server.use('/api/dishes', DishesRouter)
 
 server.get('/', (req, res) => res.send('<h2>Welcome to the API</h2>'))
 
